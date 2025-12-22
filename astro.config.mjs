64e6761ddd2a3136f10011b9e470963b3cd0a1b4
Nicolas Diaz Ruiz <ndiazruiz710@gmail.com>
2025-12-22 11:57:36 -0300

    Vercel dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   output: 'server',
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
